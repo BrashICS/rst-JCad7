cb23902f2c578500a3d6b0a5abfd622911105015
Julian Cadieux <julian.cadieux@stu.ocsb.ca>
2024-05-29 13:47:23 -0400

    latest

--- myCode/script.js
+++ myCode/script.js
@@ -5,9 +5,21 @@ Author: Julian Cadieux
 */
 'use strict'
 
-document.getElementById()
+// Learned how to get the user input from this website: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
+const form = document.querySelector("form");
+const log = document.querySelector("#log");
 
-document.querySelector('#input_id').value;
-document.querySelector('#input_id').defaultValue;
+form.addEventListener(
+  "submit",
+  (event) => {
+    const data = new FormData(form);
+    let output = "";
+    for (const entry of data) {
+      output = `${output}${entry[0]}=${entry[1]}\r`;
+    }
+    log.innerText = output;
+    event.preventDefault();
+  },
+  false,
+);
 
-console.log(value)
