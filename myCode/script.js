c07d1c5b6a4d8e4e2354a3e93e7ee2663f1bb8fe
Julian Cadieux <julian.cadieux@stu.ocsb.ca>
2024-05-30 09:52:49 -0400

    latest

--- myCode/script.js
+++ myCode/script.js
@@ -5,21 +5,28 @@ Author: Julian Cadieux
 */
 'use strict'
 
-// Learned how to get the user input from this website: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
-const form = document.querySelector("form");
-const log = document.querySelector("#log");
+
+
+// Learned how to get the user input from the "radio" and the submit button from this website: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
+
+const form = document.querySelector("form")
+const log = document.querySelector("#log")
 
 form.addEventListener(
   "submit",
   (event) => {
-    const data = new FormData(form);
-    let output = "";
+    const data = new FormData(form)
+    let output = ""
     for (const entry of data) {
-      output = `${output}${entry[0]}=${entry[1]}\r`;
+      output = `${output}${entry[0]}=${entry[1]}\r`
     }
-    log.innerText = output;
-    event.preventDefault();
+    log.innerText = output
+    event.preventDefault()
+
+    console.log(output[6])
+
+    let title = getTitle(parseInt(output[6]))
+    console.log(title)
   },
   false,
 );
-
