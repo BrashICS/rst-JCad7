f0cf188c99981a35a2b6008ca20e75e4b84b3e99
Julian Cadieux <julian.cadieux@stu.ocsb.ca>
2024-05-31 09:42:45 -0400

    latest

--- myCode/script.js
+++ myCode/script.js
@@ -5,6 +5,8 @@ Author: Julian Cadieux
 */
 'use strict'
 
+let title = ""
+
 // Learned how to get the user input from the "radio" and the submit button from this website: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
 
 const form = document.querySelector("form")
@@ -23,8 +25,21 @@ form.addEventListener(
 
     console.log(output[6])
 
-    let title = getTitle(parseInt(output[6]))
+    title = getTitle(parseInt(output[6]))
     console.log(title)
+
+    createGraphics()
   },
   false,
 );
+
+let dashes = [0]
+function createGraphics() {
+  dashes = [title.length]
+
+  for (let i = 0;i < title.length; i++) {
+    dashes.push(" _ ")
+    console.log(dashes)
+  }
+}
+
