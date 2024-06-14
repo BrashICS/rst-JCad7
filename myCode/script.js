4d3b644b66ed5ca08d996190256d98aad0aa4ea8
Julian Cadieux <julian.cadieux@stu.ocsb.ca>
2024-06-14 11:15:52 -0400

    latest

--- myCode/script.js
+++ myCode/script.js
@@ -8,10 +8,10 @@ Author: Julian Cadieux
 let title = []
 let dashes = []
 let guesses = 0
-let key = " "
 let letter = " "
-let answer =
-
+let answer = " "
+let word = " "
+let input = []
 
 document.getElementById("answerBox").addEventListener("keydown", keyTyper)
 
@@ -36,8 +36,6 @@ form.addEventListener(
     console.log(title)
 
     createGraphics()
-
-
   },
   false,
 );
@@ -53,63 +51,83 @@ function createGraphics () {
       }
       document.getElementById("underscore").innerHTML = dashes.join("")
     }
-    console.log(dashes)
 
 }
 
 
 // This function is used to listen for certain key pressess as well as weather or not the guess was right or wrong
 function keyTyper (event) {
-  document.getElementById("underscore")
-    if (document.getElementById("answerBox").value == 0 ) {
-      for (let i = 0; i < title.length; i++) {
-        answer = true
-        if (title[i] == event.key) {
-          dashes[i] = event.key
-          answer = true
-        } else if (event.key != title[i]) {
-            answer = false
-            guesses++
-              if (answer == false) {
-                if (guesses == 1) {
-                  console.log(guesses)
-                  document.getElementById("body1").hidden = !document.getElementById("body1").hidden
-                }
-                if (guesses == 2) {
-                  console.log(guesses)
-                  document.getElementById("body1").hidden = true
-                  document.getElementById("body2").hidden = !document.getElementById("body2").hidden
-                }
-                if (guesses == 3) {
-                  console.log(guesses)
-                  document.getElementById("body2").hidden = true
-                  document.getElementById("body3").hidden = !document.getElementById("body3").hidden
-                }
-                if (guesses == 4) {
-                  console.log(guesses)
-                  document.getElementById("body3").hidden = true
-                  document.getElementById("body4").hidden = !document.getElementById("body4").hidden
-                }
-                if (guesses == 5) {
-                  console.log(guesses)
-                  document.getElementById("body4").hidden = true
-                  document.getElementById("body5").hidden = !document.getElementById("body5").hidden
-
-                }
-                if (guesses == 6) {
-                  console.log(guesses)
-                  document.getElementById("body5").hidden = true
-                  document.getElementById("body6").hidden = !document.getElementById("body6").hidden
-                }
-              }
+  if (event.key === "Enter") {
+    letter = document.getElementById("answerbox").value
+    value = false
+    for (let i = 0; i <title.length; i++) {
+      if (title[i] == letter) {
+        input.push(letter)
+        dashes[i] = event.key
+        for (let x = 0; x < dashes.length; x++){
+          word += dashes[x]
         }
+        word = dashes;
+        document.getElementById("underscore").innerHTML = "" + dashes.join("")
+        console.log(dashes)
+        value = true
       }
-        document.getElementById("underscore").innerHTML = dashes.join("")
-        document.getElementById("answerBox").innerHTML = ""
+    }
+    if (value = false) {
+      guesses++;
+    }
+    if (guesses == 1) {
+      console.log(guesses)
+      document.getElementById("body1").hidden = !document.getElementById("body1").hidden
+    }
+    if (guesses == 2) {
+      console.log(guesses)
+      document.getElementById("body1").hidden = true
+      document.getElementById("body2").hidden = !document.getElementById("body2").hidden
+    }
+    if (guesses == 3) {
+      console.log(guesses)
+      document.getElementById("body2").hidden = true
+      document.getElementById("body3").hidden = !document.getElementById("body3").hidden
+    }
+    if (guesses == 4) {
+      console.log(guesses)
+      document.getElementById("body3").hidden = true
+      document.getElementById("body4").hidden = !document.getElementById("body4").hidden
+    }
+    if (guesses == 5) {
+      console.log(guesses)
+      document.getElementById("body4").hidden = true
+      document.getElementById("body5").hidden = !document.getElementById("body5").hidden
 
     }
+    if (guesses == 6) {
+      console.log(guesses)
+      document.getElementById("body5").hidden = true
+      document.getElementById("body6").hidden = !document.getElementById("body6").hidden
 
-  console.log(event.key)
+    }
+  }
+
+
+  //   let answer = false
+  //   if (document.getElementById("answerBox").value == 0 ) {
+  //     for (let i = 0; i < title.length; i++) {
+  //       if (title[i] == event.key) {
+  //         dashes[i] = event.key
+  //         answer = true
+  //       }
+  //           if (answer == false) {
+  //             guesses++
+
+  //             }
+
+  //     }
+
+  //       document.getElementById("answerBox").innerHTML = ""
+
+  //   }
+  // console.log(event.key)
 }
 
 
